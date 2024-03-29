c63f397533f886bb0733f61095d1701ccbc08eb5
Lehtonj3 <lehtonj3@mail.lcc.edu>
2024-03-29 13:38:17 -0400

    I am not sure where I am going wrong it says I dont have car defined but it is literally typed out as "Car" and it says it is not defined

---
+++ car.py
@@ -0,0 +1,25 @@
+#simple car class that includes accelerating and braking capabilities
+class car:
+    def __init__(self, year_model, make):
+        #initialize the car object with the given year_model and make
+        self.__year_model = year_model
+        self.__make = make
+        #initialize the current speed to 0 
+        self.__speed = 0
+        #define the speed increment/decrement value
+        self.__jump = 5 
+    
+    #
+    def accelerate(self):
+        #increase the car's speed by the value of __jump
+        self.__speed += self.__jump
+        
+    #
+    def brake(self):
+        #decrease the cars speed by the value of __jump
+        self.__speed -= self.__jump
+        
+    #
+    def get_speed(self):
+        #return the current speed of the car
+        return self.__speed
